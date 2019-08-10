538ab1a47414af30192ce219798054581e3f5ae4
Emanuel Tesar <e.tesarr@gmail.com>
2019-08-10 12:03:07 +0200

    Using hooks instead of connect

--- lib/counter.tsx
+++ lib/counter.tsx
@@ -1,15 +1,27 @@
-import { connect } from 'react-redux'
-import { thunk as _thunk, updateCnt as _updateCnt } from './actions'
+import { useDispatch, useSelector } from 'react-redux'
+import { thunk, updateCnt } from './actions'
 import Button from '@material-ui/core/Button'
 import { State } from './state'
+import { useCallback } from 'react'
 
-const Counter = ({ cnt, updateCnt, thunk }) => {
+const Counter = () => {
+  const cnt = useSelector((state: State) => state.cnt)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <Button color="primary" variant="contained" onClick={updateCnt}>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={useCallback(() => dispatch(updateCnt()), [])}
+      >
         +
       </Button>
-      <Button color="primary" variant="contained" onClick={thunk}>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={useCallback(() => dispatch(thunk()), [])}
+      >
         async
       </Button>
       <span>{cnt}</span>
@@ -17,7 +29,4 @@ const Counter = ({ cnt, updateCnt, thunk }) => {
   )
 }
 
-export default connect(
-  (state: State) => ({ cnt: state.cnt }),
-  { updateCnt: _updateCnt, thunk: _thunk },
-)(Counter)
+export default Counter
