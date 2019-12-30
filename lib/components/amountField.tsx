06f4ee52ec19b0895ce750e45730bc12284ca067
Emanuel Tesar <e.tesarr@gmail.com>
2019-12-30 10:16:43 +0100

    Calculator near amount

--- lib/components/amountField.tsx
+++ lib/components/amountField.tsx
@@ -3,9 +3,12 @@ import Input from '@material-ui/core/Input'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import InputLabel from '@material-ui/core/InputLabel'
 import CancelIcon from '@material-ui/icons/Cancel'
+// TODO: replace with proper calculator icon when material ui will have it
+import CalculatorIcon from '@material-ui/icons/Functions'
 import { omit } from '@siegrift/tsfunct'
-import React from 'react'
+import React, { useState } from 'react'
 import NumberFormat from 'react-number-format'
+import CalculatorDialog from './calculatorDialog'
 
 interface AmountFieldProps {
   shouldValidateAmount: boolean
@@ -17,22 +20,33 @@ interface AmountFieldProps {
   currencySymbol?: string
 }
 
-const IInput = (formatProps: any & { clearAmount: () => void }) => {
+interface MuiInputProps {
+  clearAmount: () => void
+  openCalculator: () => void
+}
+
+const MuiInput = (formatProps: any & MuiInputProps) => {
   return (
     <Input
-      {...omit(formatProps, ['clearAmount'])}
+      {...omit(formatProps, ['clearAmount', 'openCalculator'])}
       id="amount-id"
       placeholder="0.00"
       value={formatProps.value}
       endAdornment={
         <InputAdornment position="end">
-          <CancelIcon
-            color="primary"
-            onClick={formatProps.clearAmount}
-            style={{
-              visibility: formatProps.value ? 'visible' : 'hidden',
-            }}
-          />
+          <>
+            <CancelIcon
+              color="primary"
+              onClick={formatProps.clearAmount}
+              style={{
+                visibility: formatProps.value ? 'visible' : 'hidden',
+              }}
+            />
+            <CalculatorIcon
+              color="primary"
+              onClick={formatProps.openCalculator}
+            />
+          </>
         </InputAdornment>
       }
     />
@@ -48,25 +62,43 @@ const AmountField = ({
   className,
   currencySymbol,
 }: AmountFieldProps) => {
+  const [showCalc, setShowCalc] = useState(false)
+  const [calcExpression, setCalcExpression] = useState('')
+
   return (
-    <FormControl
-      aria-label="amount"
-      error={shouldValidateAmount && !isValidAmount(value)}
-      className={className}
-    >
-      <InputLabel htmlFor="amount-id">{label}</InputLabel>
-      <NumberFormat
-        prefix={`${currencySymbol} `}
-        thousandSeparator=","
-        decimalScale={2}
-        fixedDecimalScale
-        allowNegative={false}
-        value={value}
-        customInput={IInput}
-        onValueChange={(values) => onChange(values.value)}
-        clearAmount={() => onChange('')}
-      />
-    </FormControl>
+    <>
+      {showCalc && (
+        <CalculatorDialog
+          calcExpression={calcExpression}
+          setCalcExpression={setCalcExpression}
+          showCalc={showCalc}
+          setShowCalc={setShowCalc}
+          setAmount={onChange}
+        />
+      )}
+      <FormControl
+        aria-label="amount"
+        error={shouldValidateAmount && !isValidAmount(value)}
+        className={className}
+      >
+        <InputLabel htmlFor="amount-id">{label}</InputLabel>
+        <NumberFormat
+          prefix={`${currencySymbol} `}
+          thousandSeparator=","
+          decimalScale={2}
+          fixedDecimalScale
+          allowNegative={false}
+          value={value}
+          customInput={MuiInput}
+          onValueChange={(values) => onChange(values.value)}
+          clearAmount={() => onChange('')}
+          openCalculator={() => {
+            setCalcExpression(value)
+            setShowCalc(true)
+          }}
+        />
+      </FormControl>
+    </>
   )
 }
 
---
+++ lib/components/calculatorDialog.tsx
@@ -0,0 +1,72 @@
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import TextField from '@material-ui/core/TextField'
+// @ts-ignore TODO: types
+import math from 'mathjs-expression-parser'
+import React from 'react'
+
+interface CalculatorDialogProps {
+  setShowCalc: (open: boolean) => void
+  showCalc: boolean
+  calcExpression: string
+  setCalcExpression: (exp: string) => void
+  setAmount: (amount: string) => void
+}
+
+const CalculatorDialog = ({
+  calcExpression,
+  setCalcExpression,
+  setShowCalc,
+  showCalc,
+  setAmount,
+}: CalculatorDialogProps) => {
+  let exprResult: number | null
+  try {
+    exprResult = math.eval(calcExpression)
+  } catch {
+    exprResult = null
+  }
+
+  return (
+    <Dialog
+      onClose={() => setShowCalc(false)}
+      aria-labelledby="simple-dialog-title"
+      open={showCalc}
+    >
+      <DialogTitle>Calculator</DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          error={exprResult === null}
+          label="Expression"
+          value={calcExpression}
+          onChange={(e) => setCalcExpression(e.target.value)}
+          helperText={
+            exprResult === null
+              ? 'Malformed expression'
+              : `Result: ${exprResult || ''}`
+          }
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setShowCalc(false)} color="primary">
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            setAmount('' + (exprResult || ''))
+            setShowCalc(false)
+          }}
+          color="primary"
+        >
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default CalculatorDialog
