689fbba541a2c41628217b3f92216f7c435ba10e
Emanuel Tesar <e.tesarr@gmail.com>
2020-09-26 10:37:43 +0200

    Implement proper cacheng (exclude firebase from cache)

--- next.config.js
+++ next.config.js
@@ -25,26 +25,134 @@ const nextConfig = {
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
   },
   transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: false,
   // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-webpack-plugin.GenerateSW#GenerateSW
   workboxOpts: {
     swDest: 'static/service-worker.js',
     runtimeCaching: [
       {
-        urlPattern: /^https?.*/,
-        // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-strategies
+        // MUST be the same as "start_url" in manifest.json
+        urlPattern: '/',
         handler: 'StaleWhileRevalidate',
         options: {
-          cacheName: 'https-calls',
+          // don't change cache name
+          cacheName: 'start-url',
           expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            maxEntries: 1,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
           },
-          cacheableResponse: {
-            statuses: [0, 200],
+        },
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
           },
+        },
+      },
+      {
+        urlPattern: /^https:\/firebaselogging-pa\.googleapis\.com\/.*/i,
+        handler: 'CacheFirst', // exchange rate
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/firebaselogging-pa\.googleapis\.com\/.*/i,
+        handler: 'NetworkOnly', // firebase can work offline
+      },
+      {
+        urlPattern: /^https:\/firestore\.googleapis\.com\/.*/i,
+        handler: 'NetworkOnly', // firebase can work offline
+      },
+      {
+        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-font-assets',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-image-assets',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-js-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-style-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 days
+          },
+        },
+      },
+      {
+        // exchange rates are valid for a single day
+        urlPattern: /\/api\.exchangeratesapi\.io\/.*$/i,
+        handler: 'CacheFirst',
+        method: 'GET',
+        options: {
+          cacheName: 'exchange-rates',
+          expiration: {
+            maxEntries: 1,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\/api\/.*$/i,
+        handler: 'NetworkFirst',
+        method: 'GET',
+        options: {
+          cacheName: 'apis',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 90 * 24 * 60 * 60, // 90 days
+          },
+          networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+        },
+      },
+      {
+        urlPattern: /.*/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'others',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10,
         },
       },
     ],
