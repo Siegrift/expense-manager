c07754f2de6c280161b99c2f8f14cdc0504ddc63
jitka1997 <jitka.mur@gmail.com>
2021-12-21 19:02:32 +0100

    Fix root redirect

--- lib/server/utils.ts
+++
@@ -1,6 +0,0 @@
-import { ServerResponse } from 'http'
-
-export const redirect = (res: ServerResponse, path: string) => {
-  res.writeHead(301, { Location: path })
-  res.end()
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,4 @@
-import { GetServerSideProps } from 'next'
-
-import { redirect } from '../lib/server/utils'
-
 // we have no landing page, redirect to /add
 const IndexPage = () => null
-
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  redirect(res, '/add')
-  return { props: {} }
-}
 
 export default IndexPage
