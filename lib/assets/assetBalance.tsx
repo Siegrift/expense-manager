a4dab817b49340e801ffb211fd160e111a14c932
Jitka Muravská <44317233+jitka1997@users.noreply.github.com>
2022-06-04 09:49:40 +0200

    New line chart in assets (#20)

---
+++ lib/assets/assetBalance.tsx
@@ -0,0 +1,143 @@
+import React, { useCallback } from 'react'
+
+import { Line } from '@nivo/line'
+import { addMonths } from 'date-fns'
+import format from 'date-fns/format'
+import { useSelector } from 'react-redux'
+
+import { CURRENCIES } from '../shared/currencies'
+import { mainCurrencySel } from '../shared/selectors'
+import { formatMoney } from '../shared/utils'
+import { DateRange } from '../types'
+
+import { recentBalanceDataSel, displayDataSel } from './selectors'
+
+const AXIS_DATE_FORMAT = 'MM.yyyy'
+const SLICE_DATE_FORMAT = 'd.MM.yyyy'
+
+interface Props {
+  width: number
+  height: number
+  dateRange?: DateRange
+}
+
+const AssetBalance = ({ width, height, dateRange }: Props) => {
+  const mainCurrency = useSelector(mainCurrencySel)
+
+  const { monthsToDisplay, xAxisMergeSize, ...computedDateRange } = useSelector(displayDataSel(width, dateRange))
+
+  const data = useSelector(recentBalanceDataSel(monthsToDisplay, computedDateRange))
+
+  if (data.length > 500) return null
+
+  let minY = -500
+  let maxY = 500
+  data.forEach((asset, i) =>
+    asset.data.forEach((point, j) => {
+      minY = i === 0 && j === 0 ? point.y : Math.min(minY, point.y)
+      maxY = i === 0 && j === 0 ? point.y : Math.max(maxY, point.y)
+    })
+  )
+
+  const showSlice = useCallback(
+    ({ slice }: any) => {
+      if (!mainCurrency) return null
+
+      return (
+        <div
+          style={{
+            background: 'white',
+            padding: '9px 12px',
+            border: '1px solid #ccc',
+          }}
+        >
+          {`Date: ${format(addMonths(computedDateRange.start, slice.points[0].data.index), SLICE_DATE_FORMAT)}`}
+          {slice.points.map((point: any) => (
+            <div
+              key={point.id}
+              style={{
+                color: point.serieColor,
+                padding: '3px 0',
+              }}
+            >
+              <strong>
+                {point.serieId + ': '}
+                {formatMoney(point.data.y, mainCurrency)}
+              </strong>
+            </div>
+          ))}
+        </div>
+      )
+    },
+    [monthsToDisplay, mainCurrency]
+  )
+
+  return (
+    <div
+      style={{
+        position: 'relative' /* to make sure the toggle absolutely positioned toggle buttons are scoped */,
+      }}
+    >
+      <Line
+        width={width}
+        height={height}
+        data={data}
+        margin={{ top: 30, right: 20, bottom: 50, left: 50 }}
+        xScale={{
+          type: 'point',
+        }}
+        enableGridX={false}
+        yScale={{
+          type: 'linear',
+          min: Math.round(minY),
+          max: Math.round(maxY),
+        }}
+        enableGridY={true}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          ticksPosition: 'before',
+          tickSize: 5,
+          tickPadding: 5,
+          format: (v) => {
+            return (v as number) % xAxisMergeSize === 0
+              ? format(addMonths(computedDateRange.start, v as number), AXIS_DATE_FORMAT)
+              : ''
+          },
+        }}
+        axisLeft={{
+          ticksPosition: 'before',
+          tickSize: 5,
+          tickPadding: 5,
+          format: (v) => `${v} ${mainCurrency ? CURRENCIES[mainCurrency].symbol : ''}`,
+        }}
+        pointSize={3}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        enableArea
+        enableSlices="x"
+        curve="monotoneX"
+        sliceTooltip={showSlice}
+        animate={false}
+        legends={[
+          {
+            anchor: 'bottom',
+            direction: 'row',
+            justify: false,
+            translateX: 0,
+            translateY: 50,
+            itemWidth: 100,
+            itemHeight: 20,
+            itemsSpacing: 5,
+            symbolSize: 10,
+            symbolShape: 'square',
+            itemDirection: 'left-to-right',
+            itemTextColor: '#777',
+          },
+        ]}
+      />
+    </div>
+  )
+}
+export default AssetBalance
--- lib/assets/selectors.ts
+++ lib/assets/selectors.ts
@@ -1,11 +1,18 @@
+import { update } from '@siegrift/tsfunct'
+import { endOfMonth, subMonths, differenceInCalendarMonths } from 'date-fns'
+import isWithinInterval from 'date-fns/isWithinInterval'
 import filter from 'lodash/filter'
 import map from 'lodash/map'
+import range from 'lodash/range'
 import reduce from 'lodash/reduce'
 import { createSelector } from 'reselect'
 
-import { Tag } from '../addTransaction/state'
+import { Tag, Transaction } from '../addTransaction/state'
 import { percentage, sorted } from '../shared/utils'
 import { State } from '../state'
+import { tagsSel } from '../tags/selectors'
+import { sortedTransactionsSel } from '../transactions/selectors'
+import { DateRange, ObjectOf } from '../types'
 
 interface AssetTagShare {
   id: string
@@ -23,14 +30,16 @@ interface AssetTagSum {
 
 export const assetTagsSumsSel = createSelector(assetTagsSel, transactionsSel, (assetTags, txs): AssetTagSum[] =>
   map(assetTags, (assetTag) => {
-    const filteredTxs = filter(txs, (tx) => tx.tagIds.includes(assetTag.id))
+    const asssetTxs = filter(txs, (tx) => tx.tagIds.includes(assetTag.id))
+    const minusTransferTxs = filter(txs, (tx) => tx.tagIds[0] === assetTag.id && tx.type === 'transfer')
+    const plusTransferTxs = filter(txs, (tx) => tx.tagIds[1] === assetTag.id && tx.type === 'transfer')
     const minusAmout = reduce(
-      filter(filteredTxs, (tx) => tx.type === 'expense'),
+      filter(asssetTxs, (tx) => tx.type === 'expense').concat(minusTransferTxs),
       (acc, tx) => acc - tx.amount,
       0
     )
     const plusAmout = reduce(
-      filter(filteredTxs, (tx) => tx.type === 'income'),
+      filter(asssetTxs, (tx) => tx.type === 'income').concat(plusTransferTxs),
       (acc, tx) => acc + tx.amount,
       0
     )
@@ -56,3 +65,141 @@ export const assetTagSharesSel = createSelector(assetTagsSumsSel, (assetTagsSums
   // descending sort
   return sorted(assetTagShares, (t1, t2) => t2.value - t1.value)
 })
+
+export const displayDataSel = (width: number, dateRange: DateRange | undefined) =>
+  createSelector(sortedTransactionsSel, () => {
+    // 55 is ad-hoc const that works well with current xAxis format (dd.MM)
+    // `width / LABEL_WIDTH_PX` is the number of labels displayed on xAxis
+    const LABEL_WIDTH_PX = 55
+
+    const endOfThisMonth = endOfMonth(new Date())
+    let range: DateRange
+    let xAxisMergeSize: number
+    let monthsToDisplay: number
+    if (dateRange) {
+      range = dateRange
+      monthsToDisplay = differenceInCalendarMonths(dateRange.end, dateRange.start) + 1
+      xAxisMergeSize = Math.round(monthsToDisplay / (width / LABEL_WIDTH_PX))
+    } else {
+      monthsToDisplay = Math.round(width / LABEL_WIDTH_PX)
+      range = { start: subMonths(endOfThisMonth, monthsToDisplay), end: endOfThisMonth }
+      xAxisMergeSize = 1 // show every label
+    }
+
+    return {
+      xAxisMergeSize: Math.max(xAxisMergeSize, 1),
+      monthsToDisplay,
+      ...range,
+    }
+  })
+
+const assetTxsSel = createSelector(assetTagsSel, transactionsSel, (assetTags, txs) =>
+  filter(txs, (tx) => tx.tagIds.some((id) => assetTags.map((assetTag) => assetTag.id).includes(id)))
+)
+
+const assetTxBelogsTo = (tx: Transaction, assetTags: Tag[], tags: ObjectOf<Tag>): string => {
+  const assetTagsIds = assetTags.map((tag) => tag.id)
+  let idOfAssetTag = ''
+  tx.tagIds.forEach((id) => {
+    if (assetTagsIds.includes(id)) idOfAssetTag = id
+  })
+  return tags[idOfAssetTag].name
+}
+
+const nameOfTag = (id: string, tags: ObjectOf<Tag>): string => tags[id].name
+
+type Lines = {
+  [key: string]: {
+    id: string
+    data: Array<{
+      x: number
+      y: number
+      index: number
+    }>
+  }
+}
+
+export const recentBalanceDataSel = (monthsToDisplay: number, dateRange: DateRange) =>
+  createSelector(assetTxsSel, assetTagsSel, tagsSel, (transactions, assetTags, tags) => {
+    interface LineChartData {
+      amount: number
+      dataIndex: number
+      type: 'income' | 'expense' | 'transfer'
+      asset: string
+    }
+
+    const assetTagsIds = assetTags.map((tag) => tag.id)
+    const expenseTrasferTransactions = transactions
+      .filter((tx) => tx.type === 'transfer' && assetTagsIds.includes(tx.tagIds[0]))
+      .map(
+        (tx): Transaction => ({
+          ...tx,
+          type: 'expense',
+          tagIds: tx.tagIds.slice(0, 1),
+        })
+      )
+    const incomeTrasferTransactions = transactions
+      .filter((tx) => tx.type === 'transfer' && assetTagsIds.includes(tx.tagIds[1]))
+      .map(
+        (tx): Transaction => ({
+          ...tx,
+          type: 'income',
+          tagIds: tx.tagIds.slice(1, 2),
+        })
+      )
+    const assets = transactions
+      .filter((tx) => tx.type !== 'transfer')
+      .concat(incomeTrasferTransactions)
+      .concat(expenseTrasferTransactions)
+
+    type LCO = {
+      [key: string]: LineChartData[]
+    }
+
+    const groupedTransactions = assets
+      .filter((tx) => isWithinInterval(tx.dateTime, dateRange))
+      .map(
+        (tx): LineChartData => ({
+          amount: tx.amount * (tx.rate ?? 1),
+          type: tx.type,
+          dataIndex: monthsToDisplay - (differenceInCalendarMonths(dateRange.end, tx.dateTime) + 1),
+          asset: tx.tagIds.length === 1 ? nameOfTag(tx.tagIds[0], tags) : assetTxBelogsTo(tx, assetTags, tags),
+        })
+      )
+      .reduce(
+        (acc, tx) => {
+          return update(acc, [tx.dataIndex, tx.asset], (d) => (d ? [...d, tx] : [tx]))
+        },
+        range(monthsToDisplay).map(() => ({} as LCO))
+      )
+
+    const data = {} as Lines
+
+    groupedTransactions.forEach((assetsInMonth, dataInd) => {
+      Object.keys(assetsInMonth).forEach((asset) => {
+        const txs = assetsInMonth[asset]
+        if (!data[asset])
+          data[asset] = {
+            id: asset,
+            data: range(monthsToDisplay).map((_, index) => ({
+              x: index,
+              y: 0,
+              index: index,
+            })),
+          }
+        txs.forEach((tx) => {
+          if (tx.type === 'expense') data[tx.asset].data[dataInd].y -= tx.amount
+          if (tx.type === 'income') data[tx.asset].data[dataInd].y += tx.amount
+        })
+      })
+    })
+
+    Object.values(data).forEach((asset) =>
+      asset.data.forEach((_, index) => {
+        if (index !== 0) asset.data[index].y += asset.data[index - 1].y
+      })
+    )
+
+    // the y values are not rounded! Make sure to round in the component
+    return Object.values(data)
+  })
